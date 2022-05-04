eb54aec772f174bbca27acb3d4f55a97ab5a5f72
Vivek <vivekcoolguy25@hotmail.com>
2022-05-04 12:24:05 +0530

    SpringBootCrud

---
+++ src/main/java/com/example/demo/service/SubjectService.java
@@ -0,0 +1,47 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.bean.Subject;
+import com.example.demo.repository.SubjectRepository;
+
+@Service
+public class SubjectService {
+
+	@Autowired
+	SubjectRepository subjectrepository;
+	@RequestMapping(method=RequestMethod.GET, value="/subjects")
+	public List<Subject> getAllSubjects() {
+		List<Subject> subjects = new ArrayList<Subject>();
+		subjectrepository.findAll().forEach(subjects::add);
+		return subjects;
+	}
+
+	@RequestMapping(method=RequestMethod.POST, value="/subjects")
+	public void addSubject(Subject subject) {
+		System.out.println(subject.getId());
+		System.out.println(subject.getName());
+		subjectrepository.save(subject);
+		
+		
+	}
+
+	@RequestMapping(method=RequestMethod.PUT, value="/subjects/{id}")
+	public void updateSubject(Integer id, Subject subject) {
+		subjectrepository.save(subject);
+		
+	}
+
+	@RequestMapping(method=RequestMethod.DELETE, value="/subjects/{id}")
+	public void deleteSubject(Integer id) {
+		subjectrepository.deleteById(id);
+		
+	}
+
+}
